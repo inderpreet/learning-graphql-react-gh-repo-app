22146f0b5e2ec966b1e7700c8dee4e1e1088d7f2
Inderpreet Singh <er.inderpreet@gmail.com>
2021-05-18 15:01:50 -0400

    Adds code to fetch from github GraphQL

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import github from "./db";
+import query from "./Query";
 
 function App() {
-  return (
-    <div className="App container mt-5">
-      <h1 className="text-primary">
-        <i className="bi bi-diagram-2-fill"></i>Repos
-      </h1>
-    </div>
-  );
+
+    let [userName, setUserName] = useState('');
+
+    useEffect(() => {
+
+
+        fetch(github.baseURL, {
+            method: "POST",
+            headers: github.headers,
+            body: JSON.stringify(query),
+        })
+            .then((resp) => resp.json())
+            .then((data) => {
+                console.log(data);
+                setUserName(data.data.viewer.name);
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+    });
+
+    return (
+        <div className="App container mt-5">
+            <h1 className="text-primary">
+                <i className="bi bi-diagram-2-fill"></i>Repos
+            </h1>
+            <p>
+                Hey there {userName}
+            </p>
+        </div>
+    );
 }
 
 export default App;
