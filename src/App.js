332dd15779a5578731bef96d86bf9c7da3130d65
Inderpreet Singh <er.inderpreet@gmail.com>
2021-05-18 13:49:07 -0400

    Basic Setup for bootstrap complete

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import React from 'react';
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App container mt-5">
+      <h1 className="text-primary">
+        <i className="bi bi-diagram-2-fill"></i>Repos
+      </h1>
+    </div>
+  );
 }
 
 export default App;
