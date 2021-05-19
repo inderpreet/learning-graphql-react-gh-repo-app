029c839ba2b68c9405c6f2fe7095102c41a70682
Inderpreet Singh <er.inderpreet@gmail.com>
2021-05-19 08:47:26 -0400

    Add Subscription Status

--- src/Query.js
+++ src/Query.js
@@ -11,6 +11,7 @@ const gitQuery = {
                   description
                   id
                   url
+                  viewerSubscription
                   }
                 }    
               }
